d038c6eac299f8b2e688e9cd2bc7c2254ecf240f
M9J <mnsh.exe@gmail.com>
2025-05-07 03:21:21 +0000

    Deploying to gh-pages from @ M9J/bookmarks@7817a222db52964b6439c21e512bd3c1c0669b0e 🚀

---
+++ bookmarks.config.js
@@ -0,0 +1,27 @@
+const CONFIG = {};
+export default CONFIG;
+
+CONFIG.faviconSize = 24;
+
+CONFIG.bookmarks = [
+  {
+    name: "Google",
+    link: "https://google.com/",
+    icon: "",
+  },
+  {
+    name: "YouTube",
+    link: "https://youtube.com/",
+    icon: "",
+  },
+  {
+    name: "Manorama Online",
+    link: "https://manoramaonline.com/",
+    icon: "",
+  },
+  {
+    name: "Copilot",
+    link: "https://copilot.microsoft.com",
+    icon: "",
+  },
+];
