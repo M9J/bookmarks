dd9e886144959e76ae0728c3e497f3ea88b8d265
Maneesh <mnsh.exe@gmail.com>
2025-05-16 16:58:17 +0530

    version fix

--- src/script.js
+++ src/script.js
@@ -180,6 +180,7 @@ async function checkForLatestVersion() {
     latestVersion = latestVersionJSON.version;
   }
   if (Number(latestVersion)) {
+    console.log(Number(latestVersion), Number(CURRENT_VERSION));
     let isNew = Number(latestVersion) > Number(CURRENT_VERSION);
     const latestVersionDiv = document.getElementById("latest-version");
     if (isNew) {
